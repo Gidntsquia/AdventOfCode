42de56ca5fc7d7cd44f97ff3c229799d02f7e581
GidntSquia <jaxondlee@gmail.com>
2022-12-06 00:34:23 -0500

    Add solution to day 6 and prepare day 7

--- Day_6/main.py
+++ Day_6/main.py
@@ -1,7 +1,7 @@
 import re
 # Written by Jaxon Lee (GidntSquia)
-# Advent of Code Day ???
-# 12/??/2022
+# Advent of Code Day 6
+# 12/6/2022
 #
 # Feel free to take whatever you'd like!
 
@@ -9,36 +9,70 @@ import re
 # nums = [int(s) for s in re.findall(r'\d+', line)]
 
     
-# 
-# This gives the answer to part 2.
+# This finds the first series of 4 different characters and returns the index of
+# the last character in that series.
+# This gives the answer to part 1.
 def part1():
-    f = open('Day_???/input.txt')
+    f = open('Day_6/input.txt')
     solution = 0
-
-    # This runs the code on all the lines of the input. 
-    for line in f.readlines():
-        pass
+    
+    # Make sure to strip the line of input to get rid of the new line character
+    # at the end.
+    test_str = f.readline().strip()
+    four_char_windows = [test_str[i: i + 4] for i in range(len(test_str))]
+    
+    flag = True
+    for my_str in four_char_windows:
+        if flag:
+            new_str = my_str
+            for i in range(4):
+                new_str, popped = new_str[:-1], new_str[-1]
+                if popped in new_str:
+                    break
+                else:
+                    if len(new_str) == 0:
+                        # Add 4 to index to get correct "ending" character value
+                        solution = four_char_windows.index(my_str) + 4
+                        flag = False
+        
+                
     
     print("Part 1 Solution: " + str(solution))
     f.close()
 
-# 
+# This finds the first series of 14 different characters and returns the index of
+# the last character in that series.
 # This gives the answer to part 2.
 def part2():
-    f = open('Day_???/input.txt')
+    f = open('Day_6/input.txt')
     solution = 0
-
-    # This runs the code on all the lines of the input. 
-    for line in f.readlines():
-        pass
     
-    print("Part 1 Solution: " + str(solution))
+    # Make sure to strip the line of input to get rid of the new line character
+    # at the end.
+    test_str = f.readline().strip()
+    fourteen_char_windows = [test_str[i: i + 14] for i in range(len(test_str))]
+    
+    flag = True
+    for my_str in fourteen_char_windows:
+        if flag:
+            new_str = my_str
+            for i in range(14):
+                new_str, popped = new_str[:-1], new_str[-1]
+                if popped in new_str:
+                    break
+                else:
+                    if len(new_str) == 0:
+                        # Add 14 to index to get correct "ending" character value
+                        solution = fourteen_char_windows.index(my_str) + 14
+                        flag = False
+            
+    
+    print("Part 2 Solution: " + str(solution))
     f.close()
 
 
 if __name__ == "__main__":
     print("hello!")
-    
     
     part1()
     part2()
---
+++ Day_7/main.py
@@ -0,0 +1,45 @@
+import re
+# Written by Jaxon Lee (GidntSquia)
+# Advent of Code Day 7
+# 12/7/2022
+#
+# Feel free to take whatever you'd like!
+
+# Commonly used command- 
+# nums = [int(s) for s in re.findall(r'\d+', line)]
+
+    
+# 
+# This gives the answer to part 1.
+def part1():
+    f = open('Day_7/input.txt')
+    solution = 0
+
+    # This runs the code on all the lines of the input. 
+    for line in f.readlines():
+        pass
+    
+    print("Part 1 Solution: " + str(solution))
+    f.close()
+
+# 
+# This gives the answer to part 2.
+def part2():
+    f = open('Day_7/input.txt')
+    solution = 0
+
+    # This runs the code on all the lines of the input. 
+    for line in f.readlines():
+        pass
+    
+    print("Part 2 Solution: " + str(solution))
+    f.close()
+
+
+if __name__ == "__main__":
+    print("hello!")
+    
+    part1()
+    part2()
+    
+    
