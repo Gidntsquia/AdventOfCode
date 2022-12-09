9beb5cf59df2323ad5e85d900ddc1c86040a18b1
GidntSquia <jaxondlee@gmail.com>
2022-12-09 01:33:40 -0500

    Improve comments and spacing

--- Day_9/main.py
+++ Day_9/main.py
@@ -19,8 +19,12 @@ def move(position, change):
 def part1():
     solution = 0
     X = [l.strip() for l in open('Day_9/input.txt')]
+    
+    # Starting positions
     h_pos = (0, 0)
     t_pos = (0, 0)
+    
+    # All potential directions
     moves = {
         'R': (0, 1),
         'L': (0, -1),
@@ -31,44 +35,55 @@ def part1():
     visited = set([])
     
     visited.add(t_pos)
-    # Four moves
+
     for commands in X:
+        # Get direction and amount we should go in that direction.
         words = commands.split()
+        
         direction = words[0]
+        
+        # Put unit "direction" into change a then iterate as many times as the 
+        # amount specifies.
         change = moves[direction]
         for i in range(0, int(words[1])):
+            # Move the head and then move the tail correspondingly.
             h_pos = move(h_pos, change)
             (hx, hy) = h_pos
             (tx, ty) = t_pos
             distance = (hx - tx, hy - ty)
             match distance:
+                # Up
                 case (2, 0):
                     t_pos = move(t_pos, moves['U'])
+                # Down 
                 case (-2, 0):
                     t_pos = move(t_pos, moves['D'])
+                # Right
                 case (0, 2):
                     t_pos = move(t_pos, moves['R'])
+                # Left
                 case (0, -2):
                     t_pos = move(t_pos, moves['L'])
+                # Up and Right
                 case (1, 2) | (2, 1):
                     t_pos = move(t_pos, moves['U'])
                     t_pos = move(t_pos, moves['R'])
+                # Down and Right
                 case (-1, 2) | (-2, 1):
                     t_pos = move(t_pos, moves['D'])
                     t_pos = move(t_pos, moves['R'])
+                # Up and Left
                 case (1, -2) | (2, -1):
                     t_pos = move(t_pos, moves['U'])
                     t_pos = move(t_pos, moves['L'])
+                # Down and Left
                 case (-1, -2) | (-2, -1):
                     t_pos = move(t_pos, moves['D'])
                     t_pos = move(t_pos, moves['L'])
                 case _:
                     pass 
             visited.add(t_pos)
-        
-            
     solution = len(visited)
-   
     
     print("Part 1 Solution: " + str(solution))
 
@@ -78,7 +93,8 @@ def part2():
     solution = 0
     X = [l.strip() for l in open('Day_9/input.txt')]
     rope_positions = [(0,0) for i in range(0, 10)]
-    print(rope_positions)
+    
+    # All potential directions
     moves = {
         'R': (0, 1),
         'L': (0, -1),
@@ -89,8 +105,8 @@ def part2():
     visited = set([])
     
     visited.add((0,0))
-    # Four moves
     for commands in X:
+        # Move the head and then move each of the tails correspondingly.
         words = commands.split()
         direction = words[0]
         change = moves[direction]
@@ -98,40 +114,46 @@ def part2():
             rope_positions[0] = move(rope_positions[0], change)
             (hx, hy) = rope_positions[0]
             
+            # Go through each of the tails and move them if needed.
             for i in range(1, 10):
-                
                 (tx, ty) = rope_positions[i]
                 distance = (hx - tx, hy - ty)
                 match distance:
+                    # Up
                     case (2, 0):
                         rope_positions[i] = move(rope_positions[i], moves['U'])
+                    # Down
                     case (-2, 0):
                         rope_positions[i] = move(rope_positions[i], moves['D'])
+                    # Right
                     case (0, 2):
                         rope_positions[i] = move(rope_positions[i], moves['R'])
+                    # Left
                     case (0, -2):
                         rope_positions[i] = move(rope_positions[i], moves['L'])
+                    # Up and Right
                     case (1, 2) | (2, 1) | (2, 2):
                         rope_positions[i] = move(rope_positions[i], moves['U'])
                         rope_positions[i] = move(rope_positions[i], moves['R'])
+                    # Down and Right
                     case (-1, 2) | (-2, 1) | (-2, 2):
                         rope_positions[i] = move(rope_positions[i], moves['D'])
                         rope_positions[i] = move(rope_positions[i], moves['R'])
+                    # Up and Left
                     case (1, -2) | (2, -1) | (2, -2):
                         rope_positions[i] = move(rope_positions[i], moves['U'])
                         rope_positions[i] = move(rope_positions[i], moves['L'])
+                    # Down adn Left
                     case (-1, -2) | (-2, -1) | (-2, -2):
                         rope_positions[i] = move(rope_positions[i], moves['D'])
                         rope_positions[i] = move(rope_positions[i], moves['L'])
                     case _:
                         pass 
+                # Update "head" to last rope's position
                 (hx, hy) = rope_positions[i]
             
             visited.add(rope_positions[9])
-        
-            
     solution = len(visited)
-
     
     print("Part 2 Solution: " + str(solution))
 
